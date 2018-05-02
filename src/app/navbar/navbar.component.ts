4ada7cb11667f0c01de7ee7dcb916019f61022a7
albertopareja <35523536+albertopareja@users.noreply.github.com>
2018-05-02 23:56:46 +0200

    Cambios y modificaciones varias en header (NavBar incluido)
    He creado en la cabecera el logo de la empresa y he incrustado el logo también en la barra. También he habilitado en la zona de usuarios una imagen de usuario. Debajo del navBar también he agregado una barra de progreso por si se utiliza a la hora de cargar listados (tengo que ver bien como utilizarla...) y ademas queda bien de separación entre el header y el body del html.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() { 
   }
+
+  ngOnInit() { }
 
 }
